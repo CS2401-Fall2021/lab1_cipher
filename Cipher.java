96cbd0b74a92221cb5e3c42143686cd7f194fc76
m-steph <86425576+m-steph@users.noreply.github.com>
2021-07-20 10:04:41 -0600

    Create Cipher.java

---
+++ Cipher.java
@@ -0,0 +1,16 @@
+class Cipher{
+  int shift;
+  public Cipher(int shift){
+    this.shift=shift;
+  }
+     // TODO: write a function that implements a cesear cipher
+  public static String encode(String s){
+    return s;
+  }
+
+  // Followup Problem1: You are at headquarters, and receive an encrypted message. 
+  // You know the number by which it has been shifted. Write a method to decrypt it.
+  public static String decode(String s){
+    return s;
+  }
+}
