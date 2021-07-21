065048870c048e8dbd009f77c2aeb19b19579f37
m-steph <86425576+m-steph@users.noreply.github.com>
2021-07-21 16:08:49 -0600

    Update and rename test.java to Test.java

--- test.java
+++
@@ -1,8 +0,0 @@
-class Test {
-  public static void main(String[] args) {
-   String messageToEncode=args[0];
-   int shift =(int)(args[0]);
-   Cipher cipher= new Cipher(shift);
-   System.out.println(cipher.encode(messageToEncode));
-  }
-}
