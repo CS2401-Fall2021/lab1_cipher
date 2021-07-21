985fc55425423b22db395099213df88f25553885
m-steph <86425576+m-steph@users.noreply.github.com>
2021-07-21 16:13:24 -0600

    Update and rename Test.java to TestEncode.java

---
+++ TestEncode.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+// Used for running automatic tests. DO NOT EDIT
+public class TestEncode {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String line = scanner.nextLine();
+    String[] inputs=line.split(",");
+    String messageToEncode=inputs[0];
+    int shift =(int)(input[1]);
+    Cipher cipher= new Cipher(shift);
+    System.out.println(cipher.encode(messageToEncode));
+  }
+}
