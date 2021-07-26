f649e30f076417c310a9dd8335fe6a26d2c8cc0e
m-steph <86425576+m-steph@users.noreply.github.com>
2021-07-26 08:29:04 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 class Main {
   public static void main(String[] args) {
+     runTests();
+  }
+  public void runTests(){
+    // Here is an example test case
    String messageToEncode="here is a top secret message";
    String sampleEncoded="khuh lv d wrs vhfuhw phvvdjh";
    int shift =3;
    Cipher cipher= new Cipher(shift);
    System.out.println("Did it work?"+cipher.encode(messageToEncode).equals(sampleEncoded));
 
-    // Bonus Followup Problem1 might want to add code here
-
+  // TODO: implement 3 more test cases, like the one above using inputs that handle the edge cases you've identified.
   }
+  
    
 }
