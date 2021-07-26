e8fc5ade0bbe572d95e0739938ef9475ca295197
m-steph <86425576+m-steph@users.noreply.github.com>
2021-07-26 07:40:06 -0600

    Update TestEncode.java

--- TestEncode.java
+++ TestEncode.java
@@ -6,7 +6,7 @@ public class TestEncode {
     String line = scanner.nextLine();
     String[] inputs=line.split(",");
     String messageToEncode=inputs[0];
-    int shift =(int)(inputs[1]);
+    int shift =Integer.parseInt(inputs[1]);
     Cipher cipher= new Cipher(shift);
     System.out.println(cipher.encode(messageToEncode));
   }
